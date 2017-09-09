d7a404a0f7a388fb915904b52b19aeed8fe3f40e
sar77 <30755061+sar77@users.noreply.github.com>
2017-09-09 23:21:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,50 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+var articleOne={
+    title:"Article one",
+    heading:"article1",
+    content:`
+                <p>
+                hi from article one content.hi from article one content
+                </p>`
+};
+function createTemplate(data){
+    var title=data.title;
+    var date=data.date;
+    var content=data.content;
+    var heading =data.heading;
+
+var htmlTemplate=`
+            <html>
+            <title>${title} One</title>
+                <meta name="viewport" content="width-device-width,initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet"/>
+               
+           
+            <body>
+                <div class="container">
+                   <div>
+                    <a href ="\">Home</a>
+                    <h3>${heading}</h3>
+                    </div>
+                    <hr/>
+                    <div>
+                     ${date}            </div>
+                     ${content}
+                    </div>
+                
+            </body>
+        </html>  `;
+        return htmlTemplate;
+
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleOne));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
