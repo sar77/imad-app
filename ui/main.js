d34e20c83d9a47cfb9cfd2d39d4e222d7cf3246c
sar77 <30755061+sar77@users.noreply.github.com>
2017-09-26 00:22:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element = document.getElementById('main-text');
 element.innerHTML='new one';
 var img = document.getElementById('madi');
 img.onclick = function() {
-    var interval=setInterval(marginLeft,20);
+    var interval=setInterval(moveLeft,20);
     
 };
