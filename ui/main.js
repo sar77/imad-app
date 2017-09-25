33100ad0e1ad8677424de8d952ba8afcfcb48d01
sar77 <30755061+sar77@users.noreply.github.com>
2017-09-26 00:19:14 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,6 @@ var element = document.getElementById('main-text');
 element.innerHTML='new one';
 var img = document.getElementById('madi');
 img.onclick = function() {
-    img.marginLeft = '100px';
+    img.style.marginLeft = '100px';
     
 };
