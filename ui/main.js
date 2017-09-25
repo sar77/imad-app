f0e987c2f687b2315a940165a5d10fec8208a13e
sar77 <30755061+sar77@users.noreply.github.com>
2017-09-25 23:41:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 alert('hi !helooooo');
-var element=document.getElementByid("main-text");
+var element=document.getElementById("main-text");
 /*var onClick-function(){
     
 }*/
