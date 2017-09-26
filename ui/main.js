a9d8ec85c11b535f89e81b308fd1a85522bf38bc
sar77 <30755061+sar77@users.noreply.github.com>
2017-09-26 15:26:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,9 +23,9 @@ button.onclick=function() {
    request.send(null);
 };
 ///submit 
-var ninput=document.getElementById("name");
+var ninput=document.getElementById('name');
 var name=ninput.value;
-var submit=document.getElementById("submit");
+var submit=document.getElementById('submit');
 submit.onclick=function(){
     //makereq to server
     // cap list of names
