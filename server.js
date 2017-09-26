7ef25935e70625de7fac7d118c92582b83ba2a79
sar77 <30755061+sar77@users.noreply.github.com>
2017-09-26 15:50:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names=[];
-app.get('/submit-ans:/name', function (req, res) {
+app.get('/submit-answer/:name', function (req, res) {
 var name=req.params.name;
 names.push(name);
 
