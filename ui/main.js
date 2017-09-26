94bd3ca999d24cbf7011002392317c4ece84f2d7
sar77 <30755061+sar77@users.noreply.github.com>
2017-09-26 22:54:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onclick=function(){
         //action
             if(request.status === 200){
                 var names=request.responseText;
-                name=JSON.parse(names);
+                names=JSON.parse(names);
                 var list='';
                  for(var i=0;i<names.length;i++){
                      list += '<li>' + names[i] + '</li>';
